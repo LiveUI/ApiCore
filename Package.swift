2124d3415ddd4f3acd98342dff16d17720569022
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-04-02 23:40:20 +0100

    bump up Imperial

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/LiveUI/ErrorsCore.git", .branch("master")),
         .package(url: "https://github.com/LiveUI/VaporTestTools.git", from: "0.1.5"),
         .package(url: "https://github.com/LiveUI/FluentTestTools.git", .branch("master")),
-        .package(url: "https://github.com/vapor-community/Imperial.git", from: "0.11.0")
+        .package(url: "https://github.com/vapor-community/Imperial.git", from: "0.11.2")
     ],
     targets: [
         .target(
