ce77d3075f7c7c444b4905f31a6a657ed4061a7a
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-05-20 11:44:22 +0100

    Update ServerIcon.swift

--- Sources/ApiCore/Libs/ServerIcon.swift
+++ Sources/ApiCore/Libs/ServerIcon.swift
@@ -13,9 +13,17 @@ import Vapor
 public class ServerIcon {
     
     public static func icon(size: IconSize = .regular, on req: Request) throws -> EventLoopFuture<Data> {
-        let fm = try req.makeFileCore()
-        let fileName = "server/image/\(size.rawValue)"
-        return try fm.get(file: fileName, on: req)
+        return try icon(exists: size, on: req).flatMap() { exists in
+            guard exists else {
+                let data = Logo.data
+                return try Logo.create(from: data, on: req).map() { _ in
+                    return data
+                }
+            }
+            let fm = try req.makeFileCore()
+            let fileName = "server/image/\(size.rawValue)"
+            return try fm.get(file: fileName, on: req)
+        }
     }
     
     public static func icon(exists size: IconSize, on req: Request) throws -> EventLoopFuture<Bool> {
